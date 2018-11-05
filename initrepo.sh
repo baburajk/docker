3f83482732abe63ed0a20c819b216d0e52815338
BABURAJ KALLARAKKAL <baburaj@Aurora-5.local>
2018-11-05 15:48:54 -0600

    Initial push

---
+++ initrepo.sh
@@ -0,0 +1,7 @@
+echo "# devops" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:BaburajKallarakkal/devops.git
+git push -u origin master
+
